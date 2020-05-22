93aaab2658f9b14a29be41f1f50399b3d69e2288
Leonard Rothacker <leonard.rothacker@googlemail.com>
2020-05-22 17:29:04 +0200

    Added makefile parser and unit tests

--- devopstemplate/makefiletemplate.py
+++ devopstemplate/makefiletemplate.py
@@ -1,39 +1,79 @@
 import re
-from collections import OrderedDict
+
+
+class MakefileSection(object):
+    """Represents a section of a Makefile
+
+    Attributes:
+        title: (declared) title of the section, string or None. None for the
+            first section that contains lines not belonging to any explicitly
+            declared section.
+        content_list: list of strings representing the lines of the section
+    """
+
+    def __init__(self, section_title, first_line=None):
+        """Initialize section with title and optional first line
+
+        Params:
+            section_title: string or None
+            first_list: string (without line break) or None. Represents first
+                text line of section (title). content_list will be empty
+                if None.
+        """
+        self.title = section_title
+        self.content_list = []
+        if first_line is not None:
+            self.content_list.append(first_line)
+
+    def append_line(self, line):
+        """Add a line to the content_list of this section
+
+        Params:
+            line: string representing a text line (without line break)
+        """
+        self.content_list.append(line)
 
 
 class MakefileTemplate(object):
 
-    def __init__(self):
-        pass
+    # def __init__(self, filepath):
+    #     with open(filepath, 'r') as fh:
+    #         content_list = fh.readlines()
+
+    #     content_list = [line.strip() for line in content_list]
+    #     self.__mk_section_list = self.parse(content_list)
 
     def parse(self, content_list):
         """Parse the Makefile template and return Makefile divided in sections.
+
+        The first section is a pseudo-section which contains all lines found
+        before the first section which is declared in the Makefile. Its section
+        title is None (object) and its contents may be represented by an empty
+        list if no such lines have been found.
 
         Params:
             contents_list: list of strings specifying the Makefile template
                 contents such that each list element corresponds to a line.
         Returns:
-            section_content_list: list of tuples
-                [(section_title, section_content_list),...]
+            mk_section_list: list of MakefileSection objects
                 defining the sections of the Makefile. Concatenating the
-                section_content_list elements results in the original inputs.
-                Note: any lines before the first section will be returned with
-                section_title None.
+                content_list elements results in the original inputs.
+                Note: any lines before the first declared section will be
+                returned with section_title None.
         """
-        # Record content lines in ordered dictionary.
-        # Whenever a line contains a section title, a new key is added to the
-        # dictionary
-        section_dict = OrderedDict()
-        # Save the current section title for convenience
-        # Initialize the dictionary with None key and an empty list value
+        # Record content lines in list of (section, content_line_list).
+        # Whenever a line contains a section title, a new item is added to the
+        # list
+        mk_section_list = []
+        # Initialize the list with None section (and an empty list,
+        # see MakefileSection class)
         # None refers to all lines before the first section
-        section_current = None
-        section_dict[section_current] = []
+        mk_section = MakefileSection(section_title=None)
+        mk_section_list.append(mk_section)
         # Regex for detecting a section title in a line
         # Matches variants of '# --- section ---' with different amounts of
         # whitespace. () groups the section title for easy access.
-        p_sec = re.compile(r'^#\s*---\s*(.*)\s*---')
+        p_sec = re.compile(r'^#\s*---\s*(.*?)\s*---')
         for line in content_list:
             # Match only matches at the *beginning* of the string
             # --> using ^ in the regex would not strictly be necessary
@@ -41,14 +81,16 @@ class MakefileTemplate(object):
             # match is None if no match line does not match regex
             if match:
                 # Extract section title (0: entire match, 1: first group, ...)
-                section_current = match.group(1)
+                section_title = match.group(1)
                 # Initialize new section
-                section_dict[section_current] = [line]
+                mk_section = MakefileSection(section_title, first_line=line)
+                mk_section_list.append(mk_section)
             else:
-                # Add line to current section
-                section_dict[section_current].append(line)
+                # Add line to current section.
+                # --> current section is last element of mk_section_list
+                mk_section_list[-1].append_line(line)
 
-        return section_dict.items()
+        return mk_section_list
 
     def generate(self, section_titles):
         """Generate Makefile with contents for specified sections
--- tests/test_makefiletemplates.py
+++ tests/test_makefiletemplates.py
@@ -1,30 +1,65 @@
 import unittest
+from itertools import chain
 from devopstemplate.makefiletemplate import MakefileTemplate
 
 
 class TestMakefileTemplate(unittest.TestCase):
 
-    def test_parse(self):
-        test_str_list = ['# this',
-                         '#  is a comment',
-                         '# --- section 1 --- sdfsd',
-                         ' var = test',
-                         ' ',
-                         '#---  section2   ---',
-                         '---sec3 test---',
-                         '# ---section  3---']
+    def setUp(self):
+        self.__test_str_list = ['# this',
+                                '#  is a comment',
+                                '# --- section 1 --- text',
+                                ' VAR = test',
+                                ' ',
+                                '#---  section2   ---',
+                                '#--sec3 test---',
+                                '# ---section  3---',
+                                '',
+                                '#---test',
+                                '#---section2---',
+                                'target: dep',
+                                '   cmd']
 
+    def __section_title_list(self):
         mk_template = MakefileTemplate()
-        content_list = mk_template.parse(test_str_list)
-        print(content_list)
-        section_none_list = ['# this',
-                             '#  is a comment']
-        section_one_list = ['# --- section 1 --- sdfsd',
-                            ' var = test',
-                            ' ']
-        section_two_list = ['#---  section2   ---',
-                            '---sec3 test---']
-        section_three_list = ['# ---section  3---']
+        mk_section_list = mk_template.parse(self.__test_str_list)
+        return [section.title for section in mk_section_list]
+
+    def __section_content_list(self):
+        mk_template = MakefileTemplate()
+        mk_section_list = mk_template.parse(self.__test_str_list)
+        return [section.content_list for section in mk_section_list]
+
+    def test_parse_len(self):
+        section_list = self.__section_title_list()
+        # Check if number of sections in input equals number of sections in
+        # output, number of sections in input must be
+        # counted manually
+        self.assertEqual(len(section_list), 5)
+
+    def test_parse_sections(self):
+        section_list = self.__section_title_list()
+        self.assertListEqual(section_list, [None,
+                                            'section 1',
+                                            'section2',
+                                            'section  3',
+                                            'section2'])
+
+    def test_parse_contents(self):
+        section_content_list = self.__section_content_list()
+        content_list = list(chain.from_iterable(section_content_list))
+        # Check if concatentation of content_lists for sections results in
+        # input content list
+        self.assertListEqual(content_list, self.__test_str_list)
+
+    def test_parse_section_contents(self):
+        sec_content_list = self.__section_content_list()
+        # Check content_lists per section
+        self.assertListEqual(sec_content_list[0], self.__test_str_list[:2])
+        self.assertListEqual(sec_content_list[1], self.__test_str_list[2:5])
+        self.assertListEqual(sec_content_list[2], self.__test_str_list[5:7])
+        self.assertListEqual(sec_content_list[3], self.__test_str_list[7:10])
+        self.assertListEqual(sec_content_list[4], self.__test_str_list[10:])
 
 
 if __name__ == "__main__":
