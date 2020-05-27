a03e8647ccbea5d599ea4b1d854ba9fe998384fd
Leonard Rothacker <leonard.rothacker@googlemail.com>
2020-05-27 03:25:37 +0200

    Added unit test for extracting version string

--- tests/test_template.py
+++ tests/test_template.py
@@ -1,4 +1,5 @@
 import unittest
+import devopstemplate
 from devopstemplate.template import DevOpsTemplate
 
 
@@ -6,6 +7,14 @@ class TestMakefileTemplate(unittest.TestCase):
 
     def test_readresources(self):
         dvt = DevOpsTemplate()
+
+    def test_version(self):
+        version = devopstemplate.__version__
+        print(f'version: {version}')
+        ver_parts = version.split('.')
+        self.assertEqual(len(ver_parts), 3)
+        for part in ver_parts:
+            self.assertGreaterEqual(int(part), 0)
 
 
 if __name__ == "__main__":
