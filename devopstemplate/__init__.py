5ee6432bd5d8695b0efd38c553b7e2b46d29d3d8
Leonard Rothacker <leonard.rothacker@googlemail.com>
2020-05-27 02:32:24 +0200

    Added basic logging inits, and pkg version

--- devopstemplate/__init__.py
+++ devopstemplate/__init__.py
@@ -1,13 +1,37 @@
-"""Initialize top-level package by defining Python logging framework
+"""Initializations on Python package level
 
-Useful formats:
+- Obtain Python package version with setuptools
+- Initialize Python logging framework
+
+Useful log message formats:
 LOGGING_FORMAT = '%(asctime)-15s: [%(name)s] %(message)s'
 LOGGING_FORMAT = '[%(name)s] %(message)s'
 LOGGING_FORMAT = '%(message)s'
 """
 
+import pkg_resources
+__version__ = pkg_resources.require(__name__)[0].version
+
 import logging
 
-LOGGING_FORMAT = '[%(name)s] %(message)s'
-logging.basicConfig(level=logging.INFO,
-                    format=LOGGING_FORMAT)
+
+def logging_debug():
+    logging_format = '%(asctime)-15s: [%(name)s] %(message)s'
+    logging.basicConfig(level=logging.DEBUG,
+                        format=logging_format)
+
+
+def logging_info():
+    logging_format = '%(message)s'
+    logging.basicConfig(level=logging.INFO,
+                        format=logging_format)
+
+
+def logging_warn():
+    logging_format = '%(message)s'
+    logging.basicConfig(level=logging.WARNING,
+                        format=logging_format)
+
+
+# Default log level: info
+logging_info()
