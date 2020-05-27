24744e8d5e6113c91fee7778a2303073dcd42863
Leonard Rothacker <leonard.rothacker@googlemail.com>
2020-05-27 03:23:15 +0200

    Support for switching log format with log level

--- devopstemplate/__init__.py
+++ devopstemplate/__init__.py
@@ -13,25 +13,27 @@ import pkg_resources
 __version__ = pkg_resources.require(__name__)[0].version
 
 import logging
+# Default log level: info
+# logging.basicConfig(level=logging.INFO,
+#                     format='%(message)s')
+LOGGING_HANDLER = logging.StreamHandler()
+logging.getLogger().addHandler(LOGGING_HANDLER)
+logging.getLogger().setLevel(logging.INFO)
 
 
 def logging_debug():
     logging_format = '%(asctime)-15s: [%(name)s] %(message)s'
-    logging.basicConfig(level=logging.DEBUG,
-                        format=logging_format)
+    logging.getLogger().setLevel(logging.DEBUG)
+    LOGGING_HANDLER.setFormatter(logging.Formatter(logging_format))
 
 
 def logging_info():
     logging_format = '%(message)s'
-    logging.basicConfig(level=logging.INFO,
-                        format=logging_format)
+    logging.getLogger().setLevel(logging.INFO)
+    LOGGING_HANDLER.setFormatter(logging.Formatter(logging_format))
 
 
 def logging_warn():
     logging_format = '%(message)s'
-    logging.basicConfig(level=logging.WARNING,
-                        format=logging_format)
-
-
-# Default log level: info
-logging_info()
+    logging.getLogger().setLevel(logging.WARNING)
+    LOGGING_HANDLER.setFormatter(logging.Formatter(logging_format))
