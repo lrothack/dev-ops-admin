25e5c691d138845f3001de3fbd13f32f365bdbfc
Leonard Rothacker <leonard.rothacker@googlemail.com>
2020-06-17 03:26:56 +0200

    Added unittest for cookiecutter sub-command

--- tests/test_main.py
+++ tests/test_main.py
@@ -60,6 +60,29 @@ class TestMain(unittest.TestCase):
 
         mock_manage.assert_called_with(args_ns)
 
+    @patch('devopstemplate.main.cookiecutter')
+    def test_parse_cookiecutter(self, mock_cookiecutter):
+        arg_list = ['cookiecutter']
+        devopstemplate.main.parse_args(arg_list)
+        args_ns = Namespace()
+        args_ns.project_dir = '.'
+        args_ns.project_name = ''
+        args_ns.project_version = '0.1.0'
+        args_ns.project_url = ''
+        args_ns.project_description = ''
+        args_ns.author_name = ''
+        args_ns.author_email = ''
+        args_ns.overwrite_exists = False
+        args_ns.skip_exists = False
+        args_ns.verbose = False
+        args_ns.quiet = False
+        args_ns.version = False
+        args_ns.dry_run = False
+        args_ns.interactive = False
+        args_ns.func = mock_cookiecutter
+
+        mock_cookiecutter.assert_called_with(args_ns)
+
 
 if __name__ == "__main__":
     unittest.main()
