133aff91319139c442b77b360f5e2095bd4a2406
Leonard Rothacker <leonard.rothacker@googlemail.com>
2020-07-04 01:27:44 +0200

    Adjusting version: 0.7.0-dev [under development]

--- devopstemplate/__init__.py
+++ devopstemplate/__init__.py
@@ -8,7 +8,7 @@ from devopstemplate.log import LoggerConfig
 
 # Version can be parsed from setup.py or managed globally,
 # e.g., with bumpversion
-__version__ = '0.7.0'
+__version__ = '0.7.0-dev'
 
 
 # Global module variable that stores a singelton of the LoggerConfig
