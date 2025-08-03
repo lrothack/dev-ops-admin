f7ffb3fd255d25f1ba9e052c6df47af65c9200b8
Leonard Rothacker <leonard.rothacker@googlemail.com>
2025-08-03 17:43:52 +0200

    FIX: make version 'dev' version: 0.10.0.dev0

--- src/devopstemplate/__init__.py
+++ src/devopstemplate/__init__.py
@@ -8,9 +8,9 @@ from devopstemplate.log import LoggerConfig
 
 # Version can be parsed from setup.py or managed globally,
 # e.g., with bumpversion
-__version__ = "0.10.0"
+__version__ = "0.10.0.dev0"
 
 
-# Global module variable that stores a singelton of the LoggerConfig
+# Global module variable that stores a singleton of the LoggerConfig
 # Can be used globally in order to adjust log behavior.
 LOGCONFIG = LoggerConfig()
